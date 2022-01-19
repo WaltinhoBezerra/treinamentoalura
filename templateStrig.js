983636172e3c4a8d365f033f66fded6bed227a72
WaltinhoBezerrra <wnico.bezerra@gmail.com>
2022-01-18 21:20:13 -0300

    EXERCICIO02 18012022

---
+++ templateStrig.js
@@ -0,0 +1,8 @@
+const nome = "Walter ";
+const sobreNome = "Bezerra";
+const nomeDoMeio = "Nicodemos ";
+const idade = 2022 - 1977;
+const cidadeDeNascimento = "São Paulo";
+const apresentacao = `Meu nome é ${nome} ${nomeDoMeio} ${sobreNome}, nascido em ${cidadeDeNascimento} e tenho ${idade} anos `;
+
+console.log(apresentacao);
